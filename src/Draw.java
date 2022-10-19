72c86c45cea0f011a7dd449c9fa9bda91602a266
lukas <lukas.dewachter@student.kuleuven.be>
2022-10-19 11:39:42 +0200

    figuren + antialiasing

--- src/Draw.java
+++ src/Draw.java
@@ -11,8 +11,20 @@ public class Draw extends JComponent{
     }
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        Rectangle2D.Double r = new Rectangle2D.Double(50,75,100,200);
+
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
+        g2d.setRenderingHints(rh);
+        Rectangle2D.Double r = new Rectangle2D.Double(50,50,540,380);
         g2d.setColor(new Color(100,149,237));
         g2d.fill(r);
+        Line2D.Double l = new Line2D.Double(100,200,200,100);
+        g2d.setColor(new Color(2,1,1));
+        g2d.draw(l);
+        Ellipse2D.Double e = new Ellipse2D.Double(200,200,8,8);
+        g2d.fill(e);
+
     }
 }
